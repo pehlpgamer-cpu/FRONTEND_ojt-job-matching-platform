4f3f2968da35d8d3bfd059762e40edeceaf33644
Paulo E. Heymann <pehlpgamer@gmail.com>
2026-01-26 23:06:25 +0000

    usable components with props (nav button)

--- js/components/navButton.js
+++ js/components/navButton.js
@@ -1,7 +1,10 @@
-export function navBarButton(icon, label, link) //  highlighted, collapsed
+export function navBarButton(icon, label, link, highlighted) //  collapsed
 {
+    let highlightEffectClass = 'bg-black text-white';
+    if (!highlighted) highlightEffectClass = null;
+
     const button = ` 
-    <a href="${link}" class="p-1 pr-1.5 pl-1.5 border rounded-xl cursor-pointer hover:bg-gray-200 duration-150">
+    <a href="${link}" class="${highlightEffectClass} p-1 pr-1.5 pl-1.5 border rounded-xl cursor-pointer hover:bg-gray-200 duration-150">
             ${icon}
             <label class="cursor-pointer">${label}</label>
     </a>
