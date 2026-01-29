de2a218e14678f3f66da6a68d95475f5ec7a656d
Paulo E. Heymann <pehlpgamer@gmail.com>
2026-01-29 20:34:58 +0800

    Add collapseNavBarBtn function for navbar behavior

---
+++ js/components/collapseNavBarBtn.js
@@ -0,0 +1,11 @@
+// LOW PRIORITY 
+
+export function collapseNavBarBtn(collapseIcon, expandIcon) {
+  let btn = ''; 
+  
+  if (sessionStorage.get('') === "true") {
+    
+  } 
+
+  return btn; 
+} 
