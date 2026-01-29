b1947b38c3df1c6124aeba560d22219da7fb16f0
Paulo E. Heymann <pehlpgamer@gmail.com>
2026-01-29 21:57:19 +0000

    uhhh....

--- js/utils/toggleCollapseNavBar.js
+++ js/utils/toggleCollapseNavBar.js
@@ -1,3 +1,10 @@
 // UNSURE
 
 
+export function test()
+{
+    <button class="pl-2">
+                <i onclick="toggleCollapseNavBar()" class="bi bi-layout-sidebar-inset p-1 cursor-pointer"></i>
+                <i hidden class="bi bi-layout-sidebar-inset-reverse"></i>
+            </button>
+}
